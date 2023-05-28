3b1557b2f0c0969a517f63d40e44f1545bff506e
Edward Görlich <goerlich_e@tfs.pl>
2023-05-28 15:41:00 +0200

    configuration

--- led_line_fb3/Core/Src/main.c
+++ led_line_fb3/Core/Src/main.c
@@ -159,6 +159,7 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, LED0_Pin|LED1_Pin|LED2_Pin|LED3_Pin
