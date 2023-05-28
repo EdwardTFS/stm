9453336af75b787d5c1d2f82a446a6d893903318
Edward Görlich <goerlich_e@tfs.pl>
2023-05-28 15:40:36 +0200

    uart buffered read timeout configuration

--- uart_fb4/Core/Inc/main.h
+++ uart_fb4/Core/Inc/main.h
@@ -57,6 +57,26 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED0_Pin GPIO_PIN_0
+#define LED0_GPIO_Port GPIOB
+#define LED1_Pin GPIO_PIN_1
+#define LED1_GPIO_Port GPIOB
+#define LED2_Pin GPIO_PIN_2
+#define LED2_GPIO_Port GPIOB
+#define LED3_Pin GPIO_PIN_3
+#define LED3_GPIO_Port GPIOB
+#define LED4_Pin GPIO_PIN_4
+#define LED4_GPIO_Port GPIOB
+#define LED5_Pin GPIO_PIN_5
+#define LED5_GPIO_Port GPIOB
+#define LED6_Pin GPIO_PIN_6
+#define LED6_GPIO_Port GPIOB
+#define LED7_Pin GPIO_PIN_7
+#define LED7_GPIO_Port GPIOB
+#define LED8_Pin GPIO_PIN_8
+#define LED8_GPIO_Port GPIOB
+#define LED9_Pin GPIO_PIN_9
+#define LED9_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
--- uart_fb4/Core/Src/app_main.c
+++ uart_fb4/Core/Src/app_main.c
@@ -8,56 +8,83 @@
 
 #include "main.h"
 #include "usart.h"
+#include "gpio.h"
 #include <string.h>
 
 #define LINE_MAX_LENGTH	80
 #define LINE_BUFFER_OVERRUN -1
-#define LINE_INCOMPLETE 0
-#define LINE_OK 1
+#define LINE_OK 0
 
 static char line_buffer[LINE_MAX_LENGTH + 1];
 static uint32_t line_length;
+static const char newline[] = "\r\n";
+static const char overrun_message[] = "Przepełnienie\r\n";
+static const char unknown_message[] = "Nieznane polecenie\r\n";
 
-int line_append(uint8_t value)
+typedef struct {
+	GPIO_TypeDef* port;
+	uint16_t pin;
+}pin_t;
+
+static const pin_t LED[] = {
+		{ LED0_GPIO_Port, LED0_Pin },
+		{ LED1_GPIO_Port, LED1_Pin },
+		{ LED2_GPIO_Port, LED2_Pin },
+		{ LED3_GPIO_Port, LED3_Pin },
+		{ LED4_GPIO_Port, LED4_Pin },
+		{ LED5_GPIO_Port, LED5_Pin },
+		{ LED6_GPIO_Port, LED6_Pin },
+		{ LED7_GPIO_Port, LED7_Pin },
+		{ LED8_GPIO_Port, LED8_Pin },
+		{ LED9_GPIO_Port, LED9_Pin },
+};
+static const unsigned int arr_len = sizeof(LED) /sizeof(LED[0]);
+
+int read_line()
 {
-	if (value == '\r' || value == '\n') {
-		// odebraliśmy znak końca linii
-		if (line_length > 0) {
-			// jeśli bufor nie jest pusty to dodajemy 0 na końcu linii
-			line_buffer[line_length] = '\0';
-			// przetwarzamy dane
-			// zaczynamy zbieranie danych od nowa
-			line_length = 0;
-			return LINE_OK;
-		}
+	uint8_t value;
+	line_length = 0;
+	while(line_length < LINE_MAX_LENGTH){
+		 if(HAL_UART_Receive(&huart2, &value, 1, 1) == HAL_OK){
+			 if (value == '\r' || value == '\n') {
+				 // odebraliśmy znak końca linii
+				 if (line_length > 0) {
+					 // jeśli bufor nie jest pusty to dodajemy 0 na końcu linii
+					 line_buffer[line_length] = '\0';
+					 return LINE_OK;
+				 }
+			 }
+			 line_buffer[line_length++] = value;
+		 }
+	}
+	return LINE_BUFFER_OVERRUN;
+}
 
-		return LINE_INCOMPLETE;
+void led_set(unsigned int led, GPIO_PinState state)
+{
+	unsigned int led_inrange = led % arr_len;
+	HAL_GPIO_WritePin(LED[led_inrange].port, LED[led_inrange].pin, state);
+}
+
+void process_command(void){
+	if (strcmp(line_buffer, "ON") == 0) {
+		led_set(0, GPIO_PIN_SET);
+		return;
 	}
-	else {
-		if (line_length >= LINE_MAX_LENGTH) {
-			// za dużo danych, usuwamy wszystko co odebraliśmy dotychczas
-			line_length = 0;
-			return LINE_BUFFER_OVERRUN;
-		}
-		// dopisujemy wartość do bufora
-		line_buffer[line_length++] = value;
-		return LINE_INCOMPLETE;
+	if (strcmp(line_buffer, "OFF") == 0) {
+		led_set(0, GPIO_PIN_RESET);
+				return;
 	}
+	 HAL_UART_Transmit(&huart2, (const uint8_t*)unknown_message, strlen(unknown_message), HAL_MAX_DELAY);
 }
 
 void setup(){
 
 }
-const char newline[] = "\r\n";
-const char overrun_message[] = "Przepełnienie\r\n";
+
 void loop(){
-	  uint8_t value;
-	  int line_append_status = LINE_INCOMPLETE;
-	  while(line_append_status == LINE_INCOMPLETE){
-		  if (HAL_UART_Receive(&huart2, &value, 1, 0) == HAL_OK)
-			  line_append_status = line_append(value);
-	  }
-	  switch(line_append_status){
+	  int read_line_status = read_line();
+	  switch(read_line_status){
 	  	  case LINE_OK:
 	  		  HAL_UART_Transmit(&huart2, (const uint8_t*)line_buffer, strlen(line_buffer), HAL_MAX_DELAY);
 	  		  break;
@@ -66,5 +93,5 @@ void loop(){
 	  		  break;
 	  }
 	  HAL_UART_Transmit(&huart2, (const uint8_t*)newline, strlen(newline), HAL_MAX_DELAY);
-
+	  process_command();
 }
